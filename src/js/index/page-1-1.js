c43ecc09c26f3a2f44f2c9007542c70bd2757220
sutaojie <sutaojie@gmail.com>
2018-08-03 10:27:45 +0800

    用户界面显示歌曲名列表

---
+++ src/js/index/page-1-1.js
@@ -0,0 +1,15 @@
+{
+    let view = {
+        el : '.playlists'
+    }
+    let model = {
+
+    }
+    let controller = {
+        init(view, model){
+            this.view = view
+            this.model = model
+        }
+    }
+    controller.init(view, model)
+}
--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -1,37 +1,51 @@
 {
-    let view = {
-        el:'.page-1',
-        init(){
-            this.$el = $(this.el)
-        },
-        show(){
-            this.$el.addClass('active')
-        },
-        hide(){
-            this.$el.removeClass('active')
+  let view = {
+    el: ".page-1",
+    init() {
+      this.$el = $(this.el);
+    },
+    show() {
+      this.$el.addClass("active");
+    },
+    hide() {
+      this.$el.removeClass("active");
+    }
+  };
+  let model = {};
+  let controller = {
+    init(view, model) {
+      this.view = view;
+      this.model = model;
+      this.view.init();
+      this.bindEvents();
+      this.loadModule1();
+      this.loadModule2();
+    },
+    bindEvents() {
+      window.eventHub.on("selectTab", tabName => {
+        if (tabName === "page-1") {
+          this.view.show();
+        } else {
+          this.view.hide();
         }
+      });
+    },
+    loadModule1() {
+      let script1 = document.createElement("script");
+      script1.src = "./js/index/page-1-1.js";
+      script1.onload = function() {
+        console.log("模块一加载成功");
+      };
+      document.body.appendChild(script1);
+    },
+    loadModule2() {
+      let script2 = document.createElement("script");
+      script2.src = "./js/index/page-1-2.js";
+      script2.onload = function() {
+        console.log("模块二加载成功");
+      };
+      document.body.appendChild(script2);
     }
-    let model={
-
-    }
-    let controller = {
-        init(view, model){
-            this.view = view
-            this.model = model
-            this.view.init()
-            this.bindEvents()
-        },
-        bindEvents(){
-            window.eventHub.on('selectTab', (tabName)=>{
-                if(tabName === 'page-1'){
-                    this.view.show()
-                }else{
-                    this.view.hide()
-                }
-
-            })
-        }
-
-    }
-    controller.init(view, model)
+  };
+  controller.init(view, model);
 }
