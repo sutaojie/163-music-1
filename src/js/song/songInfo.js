f609818b16f8e9393b56ba547e7174756c458234
sutaojie <sutaojie@gmail.com>
2018-08-03 12:33:06 +0800

    播放，暂停 音频

--- src/js/song/songInfo.js
+++ src/js/song/songInfo.js
@@ -2,11 +2,22 @@
     let view = {
         el:'#app',
         template:`
-        <audio autoplay controls src="{{url}}"></audio> 
+        <audio src="{{url}}"></audio> 
+        <div class="play"><button>播放</button></div>
+        <div class="pause"><button>暂停</button></div>
         `,
         render(data){
             $(this.el).html(this.template.replace('{{url}}', data.url))
+        },
+        play(){
+            let audio = $(this.el).find('audio')[0]
+            audio.play()
+        },
+        pause(){
+            let audio = $(this.el).find('audio')[0]
+            audio.pause()
         }
+        
     } 
     let model = {
         data:{
@@ -36,8 +47,16 @@
             let id = this.getSongId()
             this.model.get(id).then(()=>{
                 this.view.render(this.model.data)
-                
-            })           
+            }) 
+            this.bindEvents()
+        },
+        bindEvents(){
+            $(this.view.el).on('click','.play', ()=>{
+                this.view.play()
+            })
+            $(this.view.el).on('click','.pause', ()=>{
+                this.view.pause()
+            })
         },
         getSongId(){
             let search = window.location.search;
