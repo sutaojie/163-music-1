136def93b8a7cd5038d8695ae4eba8fb6d82a87e
sutaojie <sutaojie@gmail.com>
2018-08-03 11:37:03 +0800

    MVC 改造一下

--- src/js/song/songInfo.js
+++ src/js/song/songInfo.js
@@ -1,20 +1,43 @@
 {
-  let search = window.location.search;
-  if (search.indexOf("?") === 0) {
-    search = search.substring(1);
-  }
-  let id = ''
-  let array = search.split('&').filter((v=>v))
-  for(let i=0; i<array.length; i++){
-      let kv = array[i].split('=')
-      let key = kv[0]
-      let value = kv[1]
-      if(key === 'id'){
-        id = value
-        break;
-      }
-      
-  }
-  console.log(id);
+    let view = {
+
+    } 
+    let model = {
+        
+    }
+    let controller = {
+        init(view, model){
+            this.view = view
+            this.model = model
+            let id = this.getSongId()
+            console.log(id);
+            
+        },
+        getSongId(){
+            let search = window.location.search;
+            if (search.indexOf("?") === 0) {
+              search = search.substring(1);
+            }
+            let id = ''
+            let array = search.split('&').filter((v=>v))
+            for(let i=0; i<array.length; i++){
+                let kv = array[i].split('=')
+                let key = kv[0]
+                let value = kv[1]
+                if(key === 'id'){
+                  id = value
+                  break;
+                }
+                
+            }
+           return id
+        }
+    }
+    controller.init(view, model)
+
+
+
+
+  
   
 }
